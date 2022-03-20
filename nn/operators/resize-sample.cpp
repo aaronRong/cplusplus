e0babc08ba764874831bb0f2d8a80a52ebb1286c
Aaron Gao <Aaron.Gao@armchina.com>
2022-03-20 21:48:34 +0800

    add resize-sample with memcpy and add cmakelist

--- nn/operators/resize-sample.cpp
+++ nn/operators/resize-sample.cpp
@@ -51,12 +51,45 @@ void upsample_int(const vector<int>& src,
     }
 }
 
+void upsample_int(/*const*/ int* src,
+                  int* dst,
+                  int h_in,
+                  int w_in,
+                  int h_scale,
+                  int w_scale)
+{
+    int* psrc = src;
+    int* pdst = dst;
+    for (int _h_in = 0; _h_in < h_in; ++_h_in)
+    {
+        for (int _w_scale = 0; _w_scale < w_scale; ++_w_scale)
+        {
+            memcpy(pdst, psrc, sizeof(int) * w_in);
+            pdst += w_in;
+        }
+        for (int _h_scale = 0; _h_scale < h_scale - 1; ++_h_scale)
+        {
+            memcpy(pdst, pdst - w_scale * w_in, sizeof(int) * w_in * w_scale);
+        }
+        pdst += (h_scale - 1) * w_scale * w_in;
+        psrc += w_in;
+    }
+}
+
 int main()
 {
-    vector<int> src = {1, 2, 3, 4};
-    vector<int> dst;
+    // vector<int> src = {1, 2, 3, 4};
+    // vector<int> dst;
+    // upsample_int(src, dst, 2, 2, 2, 3);
+    // cout << "result: ";
+    // for_each(dst.begin(), dst.end(), [](int i){ cout << i << " "; });
+
+    int src[] = {1, 2, 3, 4};
+    int dst[4 * 2 * 3];
     upsample_int(src, dst, 2, 2, 2, 3);
-    cout << "result: ";
-    for_each(dst.begin(), dst.end(), [](int i){ cout << i << " "; });
+    for (int i = 0; i < 4 * 2 * 3; ++i)
+    {
+        cout << dst[i] << " ";
+    }
     return 0;
 }
